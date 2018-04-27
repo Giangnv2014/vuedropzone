e4a4e71191f4d2ae4e44707d1891f1bc373ac61f
GiangNV <nguyen.van.giang@framgia.com>
2018-04-27 09:25:03 +0700

    Upload image with vue-dropzone

---
+++ src/components/FileUpload.vue
@@ -0,0 +1,25 @@
+<template>
+ <div id="app2">
+   <vue-dropzone id="drop1" :options="config" @vdropzone-complete="afterComplete"></vue-dropzone>
+ </div>
+</template>
+
+<script>
+import vueDropzone from "vue2-dropzone";
+
+export default {
+ data: () => ({
+   config: {
+     url: "http://localhost:8000/api/image",
+   }
+ }),
+ components: {
+   vueDropzone
+ },
+ methods: {
+   afterComplete(file) {
+     console.log(file);
+   }
+ }
+};
+</script>
